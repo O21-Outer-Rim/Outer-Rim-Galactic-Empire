3f2404b27760f1c109fcb4685bc64a59f32ec9c9
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-07-01 17:00:04 +0100

    1.6.9308

--- 1.6/Source/OuterRimGalacticEmpire/Properties/AssemblyInfo.cs
+++ 1.6/Source/OuterRimGalacticEmpire/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OuterRimGalacticEmpire")]
+[assembly: AssemblyTitle("Outer Rim - Galactic Empire")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OuterRimGalacticEmpire")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("Outpost 21")]
+[assembly: AssemblyProduct("Outer Rim - Galactic Empire")]
+[assembly: AssemblyCopyright("Copyright © Neronix17 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.6.*")]
